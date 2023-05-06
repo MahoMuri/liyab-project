79583ed4049aade5e5197ac96fff6b44f2970c2e
MahoMuri <paolo.santos_12@yahoo.com.ph>
2023-05-06 16:32:53 +0800

    added components and bg image

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import NavBar from "./components/NavBar.tsx";
+import Footer from "./components/Footer.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <div className="flex flex-col min-h-screen">
+      <NavBar />
+      <div className="flex-1">
+        <App />
+      </div>
+      <Footer />
+    </div>
+  </React.StrictMode>
+);
